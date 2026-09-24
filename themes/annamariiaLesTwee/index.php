976769bf1ee9c6d51d555649636359d3485fd14b
AnnaMaria Borosovska <40987@ma-web.nl>
2026-09-24 10:01:26 +0200

    index.php de basis elementen geplaats

---
+++ themes/annamariiaLesTwee/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Welkom op de portfolio site van <?php bloginfo('name'); ?>!</h1>
+    <p><?php bloginfo('description'); ?></p>
+</main>
+
+<?php get_footer(); ?>
